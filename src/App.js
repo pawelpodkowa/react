11c06eb16dcddb066d25bd343b1b6ee628d852d6
pawelpodkowa <ppodkowa@gmail.com>
2017-09-03 10:36:26 +0200

    error #1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+class Welcome extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <input value={this.props.name} onChange={this.handleChange} type="text"/>
+        <button type="submit" value="Submit">Go!</button>
       </div>
-    );
+    )
   }
 }
- 
+class Render extends React.Component {
+  render() {
+    return (
+      <div>
+      <h2>Witaj! {this.props.name}</h2>
+      <p> Teraz możesz dodawać swoje zadania, usówać i zmieniąć. Zadania zostaną zapisane automatycznie do localStorage. </p>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {name: localStorage.getItem('name')};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+	componentDidUpdate() {
+    localStorage.setItem('name', this.state.name);
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.name});
+  }
+  handleSubmit(event) {
+    this.setState({name: event.target.name});
+  }
+
+
+  render() {
+    return (
+      <div className="row">
+        <div className="container">
+          <div className="col-md-12">
+          <form onSubmit={this.handleSubmit}>
+          <p>przywitaj się <Welcome /></p>
+          <Render name={this.props.name} />
+          </form>
+          </div>
+                <div className="col-md-6">
+
+                </div>
+                <div className="col-md-6">
+
+                </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+
 export default App;
